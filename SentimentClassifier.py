d99d2da4849e6093d8bca47eeb62ed811da0dfaa
Javier Moreno <a01750400@tec.mx>
2022-11-18 17:34:56 -0600

    fix double underscores

--- SentimentClassifier.py
+++ SentimentClassifier.py
@@ -12,32 +12,32 @@ class SentimentClassifier:
         receives a filename from which it will source the
         sentences to be classified.
         '''
-        self.__analyzer = create_analyzer(task="sentiment", lang="en")
-        self.__lines = []
-        self.__analyses = []
-        self.__read_txt(filename)
-        self.__analyze_sentences()
-        self.__output_analyses()
+        self._analyzer = create_analyzer(task="sentiment", lang="en")
+        self._lines = []
+        self._analyses = []
+        self._read_txt(filename)
+        self._analyze_sentences()
+        self._output_analyses()
 
-    def __read_txt(self, filename: str):
+    def _read_txt(self, filename: str):
         '''
         Reads a text file and separates the content by lines.
         '''
         with open(filename) as file:
             self.__lines = [line.rstrip() for line in file]
 
-    def __analyze_sentence(self, sentence: str):
+    def _analyze_sentence(self, sentence: str):
         '''
         Calls the analyzer's predict function with the line of text received.
         '''
-        return self.__analyzer.predict(sentence)
+        return self._analyzer.predict(sentence)
 
-    def __analyze_sentences(self):
+    def _analyze_sentences(self):
         '''
         Classifies each sentence from the text file.
         '''
         for sentence in self.__lines:
-            analisis = self.__analyze_sentence(sentence)
+            analisis = self._analyze_sentence(sentence)
             result = analisis.output
             if result == 'POS':
                 res = 'POSITIVE'
@@ -48,12 +48,12 @@ class SentimentClassifier:
                     res = 'POSITIVE'
                 else:
                     res = 'NEGATIVE'
-            self.__analyses.append(res)
+            self._analyses.append(res)
 
-    def __output_analyses(self):
+    def _output_analyses(self):
         '''
         Outputs the results.
         '''
-        for analisis in self.__analyses:
+        for analisis in self._analyses:
             print(analisis)
 
