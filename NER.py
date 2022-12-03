596b267e006b29993df9f4873e3e0da59f9453df
Javier Moreno <a01750400@tec.mx>
2022-12-03 16:16:49 -0600

    docstrings

--- NER.py
+++ NER.py
@@ -6,8 +6,14 @@ from flair.trainers import ModelTrainer
 from flair.visual.training_curves import Plotter
 
 class NER:
+    '''
+    Wrapper object for the flair library
+    '''
 
     def __init__(self, *, useAllData: bool) -> None:
+        '''
+        Initializes the object and starts the training process
+        '''
         self._useAllData = useAllData
         self._data_folder = './NERData'
 
@@ -23,6 +29,9 @@ class NER:
 
 
     def _make_corpus(self):
+        '''
+        Creates a corpus object with the datasets we specify on the Corpus' parametes
+        '''
         if self._useAllData:
             train_file = 'train'
         else:
@@ -35,6 +44,9 @@ class NER:
 
 
     def _initialize_embeddings(self):
+        '''
+        Declares the embeddings to be used by our training
+        '''
         embedding_types = [
 
             WordEmbeddings('glove'),
@@ -48,6 +60,9 @@ class NER:
 
 
     def _initialize_sequence_tagger(self):
+        '''
+        Creates the tagger object that will be used in the training
+        '''
         self._tagger = SequenceTagger(hidden_size=256,
                                 embeddings=self._embeddings,
                                 tag_dictionary=self._tag_dictionary,
@@ -55,10 +70,16 @@ class NER:
 
 
     def _initialize_trainer(self):
+        '''
+        Creates the traininer helper object that will be used in the training
+        '''
         self._trainer = ModelTrainer(self._tagger, self._corpus)
 
 
     def _run_training(self):
+        '''
+        Runs the training function in the trainer object
+        '''
         self._trainer.train('resources/taggers/ner-english',
                       train_with_dev=True,
                       write_weights=True,
@@ -66,6 +87,9 @@ class NER:
 
 
     def _plot_results(self):
+        '''
+        Plots the results of the training
+        '''
         plotter = Plotter()
         plotter.plot_training_curves('resources/taggers/ner-english/loss.tsv')
         plotter.plot_weights('resources/taggers/ner-english/weights.txt')
